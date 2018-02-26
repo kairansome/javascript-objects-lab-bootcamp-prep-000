c239c390faa6b166943c53f7748b6c826b768370
Kai Emerson Ransome <ransomekai@gmail.com>
2018-02-26 05:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 
 function deleteFromObjectByKey (object, key) {
-  return Object.assign({}, delete object[key])
+  var newObj = Object.assign({}, delete object[key])
 }
